a38753b09c371bb2cb1d64c93392ff41b3150e7d
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-01-04 15:49:36 +0700

    Add new main "init variable"

---
+++ Inisialisasi-VariabelValue/init-value.c
@@ -0,0 +1,12 @@
+/* File : initvar.c */
+// Penulis : Hafif 
+// Mengisi variabel i yang bertype in dengan nilai 5 dan menuliskannya
+#include<stdio.h>
+int main()
+
+{ /*Kamus*/
+    int i =5; //deklarasi dan inisialisasi nilai variabel 1 dengan 5
+/* Algoritma */
+printf("Ini nilai i : %d \n", i); /* Untuk mengamati dampak inisialisasi */
+return 0;
+}
