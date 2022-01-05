1b2e24b2b10dfddbc8cbef45b2e2cafb875715db
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-01-05 15:09:18 +0700

    Added read value "character"

---
+++ Baca-Tulis/character.c
@@ -0,0 +1,16 @@
+/* File: character.c */
+/* Penulis: Hafif */
+/* Deskripsi: */
+#include<stdio.h>
+int main()
+{   /* Kamus */
+    char cc;
+    char c;
+
+    /* Algoritma */
+    printf ("hello \n");
+    printf ("baca 1 karakter saja : "); scanf("%c", &cc);
+    printf ("%c \n", cc);
+    printf ("baca 1 karakter saja: "); scanf ("%d", &c); /* Ketikkan angka 65 */
+    printf ("%d \n", c);
+}
