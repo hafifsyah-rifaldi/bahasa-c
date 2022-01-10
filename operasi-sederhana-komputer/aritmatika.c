f5f3bd7dd2440c73d819d6748f8b29a383b93799
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-01-10 19:17:19 +0700

    Added Aritmatika Operation

---
+++ operasi-sederhana-komputer/aritmatika.c
@@ -0,0 +1,27 @@
+/* File: aritmatika.c */
+/* Penulis : Hafif */
+/* Operasi aritmatika bilangan integer dan bilangan riil :
+menuliskan jumlah, hasil perkalian, pembagian, modulo dua buah variabel
+integer */
+
+#include<stdio.h>
+int main()
+{
+    /* Kamus */
+    int x=5;
+    int y=8;
+    /* Program */
+    printf ("Ini nilai x + y : %d \n", x+y);
+    printf ("Ini nilai x - y : %d \n", x-y);
+    printf ("Ini nilai x * y : %d \n", x*y);
+    printf ("Ini nilai x / y : %d \n", x/y);
+    printf ("Ini nilai x mod y : %d \n", x%y);
+
+    /* Bandingkan dengan operasi pembagian bilangan riil */
+    float a=5, b=8;
+    printf ("Ini nilai a / b : %5.2f \n", a/b);
+    printf ("Ini nilai b / a : %5.2f \n", b/a);
+
+
+    return 0;
+}
