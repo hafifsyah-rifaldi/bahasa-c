4f5e562fe17e6eb018249fdcd3982c07df03852e
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-01-10 19:20:13 +0700

    Added Some operation x+x+x and x*x+x

--- operasi-sederhana-komputer/aritmatika.c
+++ operasi-sederhana-komputer/aritmatika.c
@@ -17,6 +17,9 @@ int main()
     printf ("Ini nilai x / y : %d \n", x/y);
     printf ("Ini nilai x mod y : %d \n", x%y);
 
+    printf ("Ini nilai x+x+x : %d \n", x+x+x);
+    printf ("Ini nilai x*x+x : %d \n", x*x+x);
+
     /* Bandingkan dengan operasi pembagian bilangan riil */
     float a=5, b=8;
     printf ("Ini nilai a / b : %5.2f \n", a/b);
