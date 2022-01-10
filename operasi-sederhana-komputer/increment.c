337328e88c105c7496413dc0ca5516ba243bd429
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-01-10 20:05:30 +0700

    Added decrement operation

--- operasi-sederhana-komputer/increment.c
+++ operasi-sederhana-komputer/increment.c
@@ -6,13 +6,19 @@
 int main()
 {
 /* Kamus */
-    int i ;
+    int i, j ;
 
 /* Program */
     i = 3;
     printf ("Nilai i : %d %d \n", i, i++);
     i = 3;
     printf("%d\n", ++i); /* sebelum dicetak, nilai i ditambah 1 */
+
+/* Aktivitas kreatif */
+    j = 5;
+    printf ("Nilai j operator dekremen: %d %d \n", j, j--);
+    j = 5;
+    printf ("%d\n", --j);
 
 return 0;
 
